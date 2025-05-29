654b50e0d1119e0626f111690f6b16ba59c4d233
tata <TaWang@ashleyfurniture.com>
2025-05-29 08:57:50 +0800

    fix：mail

--- base/cmd/main.go
+++ base/cmd/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"base/common/redis"
 	"base/infrastructure/config"
 	"base/interfaces/api"
 
@@ -22,9 +21,6 @@ func main() {
 
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
-
-	redis.Init(c.Redis.Host, c.Redis.Pass)
-	defer redis.Close()
 
 	ctx := svc.NewServiceContext(c)
 	server := rest.MustNewServer(c.RestConf)
--- base/infrastructure/svc/servicecontext.go
+++ base/infrastructure/svc/servicecontext.go
@@ -2,32 +2,15 @@ package svc
 
 import (
 	"base/infrastructure/config"
-	"context"
-
-	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/go-zero/core/stores/redis"
 )
 
 type ServiceContext struct {
 	Config config.Config
-	Redis  *redis.Redis
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
-	logger := logx.WithContext(context.Background())
-	redisConf := redis.RedisConf{
-		Host: c.Redis.Host,
-		Type: c.Redis.Type,
-		Pass: c.Redis.Pass,
-		Tls:  c.Redis.Tls,
-	}
 
-	redisClient, err := redis.NewRedis(redisConf)
-	if err != nil {
-		logger.Errorf("Failed to create Redis client: %v", err)
-	}
 	return &ServiceContext{
 		Config: c,
-		Redis:  redisClient,
 	}
 }
